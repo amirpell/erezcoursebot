026b5e603da11f206b1800eac1f0391542853d96
amirpell <amirpb87@gmail.com>
2025-04-20 17:13:29 +0300

    S

--- server/index.js
+++ server/index.js
@@ -15,19 +15,10 @@ const client = new Client({
     }),
     puppeteer: {
         headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-extensions',
-            '--disable-gpu',
-            '--no-default-browser-check',
-            '--no-first-run',
-            '--disable-dev-shm-usage',
-        ],
+    
     }
 });
 
-let clientReady = false;
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
 });
@@ -35,13 +26,7 @@ client.on('qr', (qr) => {
 
 client.on('ready', () => {
    
-    clientReady = true;
-    setTimeout(() => {
-        // השאר את הקוד שלך כאן
-        clientReady = true;
         console.log('Client is ready!');
-
-    }, 5000);  // זמן המתנה של 5 שניות
    });
 
 
