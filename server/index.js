0e230f64a2a06fdb1c2a9219fb09ad34cd2e4801
amirpell <amirpb87@gmail.com>
2025-04-27 09:23:01 +0300

    S

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: false,
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
          dataPath: "./sessions"
     }
